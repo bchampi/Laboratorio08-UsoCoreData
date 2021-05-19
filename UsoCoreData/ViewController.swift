26a00e421bd5cdc21f6dc50e7d458ef97f78c445
Mac 17 <brayan.champi@tecsup.edu.pe>
2021-05-19 13:23:51 -0500

    Initial Commit

---
+++ UsoCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UsoCoreData
+//
+//  Created by Mac 17 on 5/19/21.
+//  Copyright © 2021 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
